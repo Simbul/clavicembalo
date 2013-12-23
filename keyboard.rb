c833d2681a41945dd9de495bd92213eff616f94f
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2013-12-23 12:17:22 +0000

    Support octaves in Keyboard

--- keyboard.rb
+++ keyboard.rb
@@ -6,10 +6,24 @@ class Keyboard
     | C | D | E | F | G | A | B | C | D | E | F | G | A | B |
     └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
   eos
+  SEMITONES = %w(C C# D D# E F F# G G# A Bb B)
 
   def self.press *notes
     KEYBOARD
       .gsub(/[A-Z]/i){ |keyboard_note| notes.include?(expand(keyboard_note)) ? '●' : ' ' }
+  end
+
+  def self.add_octave_to *notes
+    octave = 3
+    notes.map.with_index{ |note, idx|
+      octave += 1 if idx > 0 && SEMITONES.index(note) < SEMITONES.index(notes[idx - 1])
+      "#{note}#{octave}"
+    }
+  end
+
+  def self.press_once *notes
+    notes_with_octave = add_octave_to(*notes)
+    expanded_keyboard.gsub(/[A-Za-z][#b]?\d/) { |match| notes_with_octave.include?(match) ? '●' : ' ' }
   end
 
   def self.expand keyboard_note
@@ -21,5 +35,13 @@ class Keyboard
     when 'b' then 'Bb'
     else keyboard_note
     end
+  end
+
+  def self.expanded_keyboard
+    octaves = {}
+    KEYBOARD.gsub(/[A-Z]/i){ |kn|
+      octaves[kn] = octaves[kn].nil? ? 3 : octaves[kn] + 1
+      "#{kn}#{octaves[kn]}"
+    }.gsub(/[A-Z]/i){ |kn| expand(kn) }
   end
 end
