89ef2d9f2a7fe0bddfdfcc4047708f4f21d633ae
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2013-12-25 10:34:07 +0000

    Add Note class

---
+++ note.rb
@@ -0,0 +1,57 @@
+class Note
+  NAMES = %w(C C# D D# E F F# G G# A Bb B)
+
+  attr_reader :octave, :semitone
+
+  def initialize note
+    name, octave = note.match(/([A-Z][#b]?)(\d?)/).captures
+
+    raise ArgumentError, "#{name} is not a valid note name" unless NAMES.include?(name)
+
+    @semitone = NAMES.index(name)
+    @octave = octave.empty? ? 3 : octave.to_i
+  end
+
+  def name
+    NAMES[semitone]
+  end
+
+  def absolute_semitones
+    @octave * 12 + @semitone
+  end
+
+  def == other_note
+    octave == other_note.octave && semitone == other_note.semitone
+  end
+
+  def self.from_name_and_octave name, octave
+    Note.new("#{name}#{octave}")
+  end
+
+  def self.from_octave_and_semitone octave, semitone
+    Note.new("#{NAMES[semitone]}#{octave}")
+  end
+
+  def self.from_absolute_semitones absolute_semitones
+    octave = absolute_semitones / 12
+    semitone = absolute_semitones % 12
+    from_octave_and_semitone(octave, semitone)
+  end
+
+  def + interval
+    Note.from_absolute_semitones(absolute_semitones + interval)
+  end
+
+  def - interval
+    self + (- interval)
+  end
+
+  def to_s
+    "#{name}#{octave}"
+  end
+
+  def interval_to other_note
+    other_note.absolute_semitones - absolute_semitones
+  end
+
+end
---
+++ specs/note_spec.rb
@@ -0,0 +1,143 @@
+require_relative '../note'
+
+describe Note do
+  describe ".new" do
+    subject{ described_class.new(name) }
+
+    context "with an invalid name" do
+      let(:name){ 'Z#' }
+      it{ expect{ subject }.to raise_error ArgumentError, /name/ }
+    end
+
+    context "with a valid name" do
+      let(:name){ 'F#3' }
+
+      it{ expect(subject).to be_a Note }
+      its(:name){ should eq 'F#' }
+      its(:octave){ should eq 3 }
+      its(:semitone){ should eq 6 }
+
+      context "without an octave" do
+        let(:name){ 'F#' }
+        it "defaults to octave 3" do
+          expect(subject.octave).to eq 3
+        end
+      end
+    end
+  end
+
+  describe "#absolute_semitones" do
+    subject{ note.absolute_semitones }
+
+    context "with an arbitrary note" do
+      let(:note){ Note.new('F3') }
+
+      it "returns absolute semitones, counting octaves" do
+        expect(subject).to eq (12 * 3 + 5)
+      end
+    end
+
+    context "with the zero note" do
+      let(:note){ Note.new('C0') }
+      it{ expect(subject).to eq 0 }
+    end
+  end
+
+  describe "#==" do
+    let(:note){ Note.new('F3') }
+    subject{ note == other_note }
+
+    context "when the other note has the same octave and semitone" do
+      let(:other_note){ Note.new('F3') }
+      it{ expect(subject).to be_true }
+    end
+
+    context "when the other note does not have the same octave and semitone" do
+      let(:other_note){ Note.new('F4') }
+      it{ expect(subject).to be_false }
+    end
+  end
+
+  describe ".+" do
+    let(:note){ Note.new('F3') }
+    subject{ note + interval }
+
+    context "with an empty interval" do
+      let(:interval){ 0 }
+
+      it "returns the same note" do
+        expect(subject).to eq Note.new('F3')
+      end
+    end
+
+    context "with an octave interval" do
+      let(:interval){ 12 }
+
+      it "returns the same note, one octave higher" do
+        expect(subject).to eq Note.new('F4')
+      end
+    end
+
+    context "with an arbitrary interval" do
+      let(:interval){ 19 }
+
+      it "returns the note at the given interval above the target" do
+        expect(subject).to eq Note.new('C5')
+      end
+    end
+
+    context "with a negative interval" do
+      let(:interval){ -13 }
+
+      it "returns the note at the given interval below the target" do
+        expect(subject).to eq Note.new('E2')
+      end
+    end
+  end
+
+  describe ".-" do
+    let(:note){ Note.new('F3') }
+    subject{ note - interval }
+
+    context "with an arbitrary interval" do
+      let(:interval){ 13 }
+
+      it "returns the note at the given interval below the target" do
+        expect(subject).to eq Note.new('E2')
+      end
+    end
+  end
+
+  describe ".interval_to" do
+    let(:note){ Note.new('C3') }
+    subject{ note.interval_to(other_note) }
+
+    context "between a note and itself" do
+      let(:other_note){ Note.new('C3') }
+
+      it{ expect(subject).to eq 0 }
+    end
+
+    context "between a note and its higher octave" do
+      let(:other_note){ Note.new('C4') }
+
+      it{ expect(subject).to eq 12 }
+    end
+
+    context "between two arbitrary notes" do
+      let(:other_note){ Note.new('G3') }
+
+      it "returns the number of semitones between the notes" do
+        expect(subject).to eq 7
+      end
+    end
+
+    context "with a note below the target" do
+      let(:other_note){ Note.new('B2') }
+
+      it "returns a negative number of semitones between the notes" do
+        expect(subject).to eq -1
+      end
+    end
+  end
+end
