8155ee7b543a25dc004d5fed3470afa003a35ba4
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2014-03-06 22:10:55 +0000

    Replace Chords static class with Chord

--- lists.rb
+++ lists.rb
@@ -1,5 +1,5 @@
 require_relative 'scales'
-require_relative 'chords'
+require_relative 'chord'
 require_relative 'keyboard'
 
 class Lists
@@ -15,12 +15,12 @@ class Lists
 
   def self.print_chords_of root_note
     chord_abbrevs = Scales.chord_abbrevs_for(root_note)
-    chords = chord_abbrevs.map { |chord_abbrev| Chords.chord(chord_abbrev) }
+    chords = chord_abbrevs.map { |chord_abbrev| Chord.named(chord_abbrev) }
     numbers = %w(I ii iii IV V vi vii)
 
     puts "#{root_note} chords:"
     chords.each do |chord|
-      puts "(#{numbers.shift.rjust(3)})  #{chord_abbrevs.shift.rjust(3)}:  #{chord.join(' ')}"
+      puts "(#{numbers.shift.rjust(3)})  #{chord_abbrevs.shift.rjust(3)}:  #{chord.notes.map(&:name).join(' ')}"
     end
   end
 end
