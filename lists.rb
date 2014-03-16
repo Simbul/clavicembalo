1fbb277fe86f96c5633e5dc5963e8163d81a1983
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2014-03-16 20:42:48 +0000

    Fix helper class Lists

--- lists.rb
+++ lists.rb
@@ -1,5 +1,4 @@
-require_relative 'lib/scales'
-require_relative 'lib/chord'
+require_relative 'lib/scale'
 require_relative 'lib/keyboard'
 
 class Lists
@@ -7,20 +6,19 @@ class Lists
 
   def self.print_scales
     SEMITONES.each do |note|
-      scale = Scales.major_scale(note)
-      puts "#{note} major scale: #{scale}"
-      puts Keyboard.press_once(*scale)
+      scale = Scale.named(note, mode: :major)
+      puts "#{note} major scale: #{scale.notes.map(&:name)}"
+      puts Keyboard.press_once(*scale.notes)
     end
   end
 
   def self.print_chords_of root_note
-    chord_abbrevs = Scales.chord_abbrevs_for(root_note)
-    chords = chord_abbrevs.map { |chord_abbrev| Chord.named(chord_abbrev) }
+    chords = Scale.named(root_note).chords
     numbers = %w(I ii iii IV V vi vii)
 
     puts "#{root_note} chords:"
     chords.each do |chord|
-      puts "(#{numbers.shift.rjust(3)})  #{chord_abbrevs.shift.rjust(3)}:  #{chord.notes.map(&:name).join(' ')}"
+      puts "(#{numbers.shift.rjust(3)})  #{chord.name.rjust(3)}:  #{chord.notes.map(&:name).join(' ')}"
     end
   end
 end
