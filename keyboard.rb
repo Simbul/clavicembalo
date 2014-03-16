338e14a24cea0122ac52a06d3281e255b76992ca
Alessandro Morandi <alessandro.morandi@simplybusiness.co.uk>
2014-03-16 20:31:36 +0000

    Refactor Keyboard methods to accept Note class
    Module has been DRYed up as a side effect.

--- keyboard.rb
+++ keyboard.rb
@@ -8,22 +8,21 @@ class Keyboard
   eos
   SEMITONES = %w(C C# D D# E F F# G G# A Bb B)
 
-  def self.press *notes
-    KEYBOARD
-      .gsub(/[A-Z]/i){ |keyboard_note| notes.include?(expand(keyboard_note)) ? '●' : ' ' }
-  end
-
-  def self.add_octave_to *notes
-    octave = 3
-    notes.map.with_index{ |note, idx|
-      octave += 1 if idx > 0 && SEMITONES.index(note) < SEMITONES.index(notes[idx - 1])
-      "#{note}#{octave}"
-    }
+  def self.press_all *notes
+    press(*notes, with_octave: false)
   end
 
   def self.press_once *notes
-    notes_with_octave = add_octave_to(*notes)
-    expanded_keyboard.gsub(/[A-Za-z][#b]?\d/) { |match| notes_with_octave.include?(match) ? '●' : ' ' }
+    press(*notes, with_octave: true)
+  end
+
+
+  private
+
+  def self.press *notes, with_octave: false
+    note_names = notes.map{ |n| n.name(with_octave: with_octave) }
+    expanded_keyboard(with_octave: with_octave)
+      .gsub(/[A-Za-z][#b]?\d?/){ |match| note_names.include?(match) ? '●' : ' ' }
   end
 
   def self.expand keyboard_note
@@ -37,11 +36,19 @@ class Keyboard
     end
   end
 
-  def self.expanded_keyboard
+  def self.expanded_keyboard(with_octave: false)
     octaves = {}
-    KEYBOARD.gsub(/[A-Z]/i){ |kn|
-      octaves[kn] = octaves[kn].nil? ? 3 : octaves[kn] + 1
-      "#{kn}#{octaves[kn]}"
-    }.gsub(/[A-Z]/i){ |kn| expand(kn) }
+    keyboard = KEYBOARD.dup
+
+    if with_octave
+      keyboard.gsub!(/[A-Z]/i){ |kn|
+        octaves[kn] = octaves[kn].nil? ? 3 : octaves[kn] + 1
+        "#{kn}#{octaves[kn]}"
+      }
+    end
+
+    keyboard.gsub!(/[A-Z]/i){ |kn| expand(kn) }
+
+    keyboard
   end
 end
---
+++ specs/keyboard_spec.rb
@@ -0,0 +1,108 @@
+require_relative '../lib/note'
+require_relative '../keyboard'
+
+describe Keyboard do
+  describe ".press_all" do
+    subject{ described_class.press_all(*notes) }
+
+    context 'with a single C note' do
+      let(:notes){ [Note.new('C')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    | ● |   |   |   |   |   |   | ● |   |   |   |   |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+
+    context 'with a single C# note' do
+      let(:notes){ [Note.new('C#')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  |●| | |  |  | | | | | |  |  |●| | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+
+    context 'with a C major chord' do
+      let(:notes){ [Note.new('C'), Note.new('E'), Note.new('G')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    | ● |   | ● |   | ● |   |   | ● |   | ● |   | ● |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+  end
+
+  describe ".press_once" do
+    subject{ described_class.press_once(*notes) }
+
+    context 'with a single C note' do
+      let(:notes){ [Note.new('C')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    | ● |   |   |   |   |   |   |   |   |   |   |   |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+
+    context 'with a single C# note' do
+      let(:notes){ [Note.new('C#')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  |●| | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+
+    context 'with a C major chord' do
+      let(:notes){ [Note.new('C'), Note.new('E'), Note.new('G')] }
+
+      it 'renders correctly' do
+        expect(subject).to eq(
+  <<-eos
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  | | | |  |  | | | | | |  |  | | | |  |  | | | | | |  |
+    |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |  └┬┘ └┬┘  |  └┬┘ └┬┘ └┬┘  |
+    | ● |   | ● |   | ● |   |   |   |   |   |   |   |   |   |
+    └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+  eos
+        )
+      end
+    end
+  end
+end
